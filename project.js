193e92896567065ca26195dd9fa4d1dbcfd8dba3
Hiba Maala <hiba.maala@gmail.com>
2019-09-18 15:31:30 +0200

    new v.20190918

---
+++ project.js
@@ -0,0 +1,23 @@
+
+function changeText(id) {
+    id.innerHTML = "Tack för att ni har lyssnat!";
+    id.style.color = "red";
+}
+
+const arrayOfHeaders = document.getElementsByTagName("p");
+for (const header of arrayOfHeaders) {
+    header.style.color = "maroon";
+
+}
+
+function change_pic() {
+    var imgObj = document.getElementById("meis_pic");
+    if (imgObj.getAttribute("src", 2) == "images/10.jpg") {
+        imgObj.src = "images/2.png";
+    }
+    else { imgObj.src = "images/10.jpg"; }
+}
+
+
+meis_pic.style.width = "100%";
+meis_pic.style.height = "auto";
