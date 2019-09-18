ac988b1cb4d0a054f454e0f316e39e83d9c98922
Hiba Maala <hiba.maala@gmail.com>
2019-09-18 14:39:27 +0200

    last version 20190918

---
+++ projectHiba.js
@@ -0,0 +1,7 @@
+function onFunction() {
+    document.getElementById("colorImportant").style.color = "red";
+}
+
+function offFunction() {
+    document.getElementById("colorImportant").style.color = "black";
+}
