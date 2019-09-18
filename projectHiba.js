6adbd35fab09de29a19b5571f243c1cfc860205e
Hiba Maala <hiba.maala@gmail.com>
2019-09-18 15:29:25 +0200

    Delete 2

--- projectHiba.js
+++
@@ -1,7 +0,0 @@
-function onFunction() {
-    document.getElementById("colorImportant").style.color = "red";
-}
-
-function offFunction() {
-    document.getElementById("colorImportant").style.color = "black";
-}
